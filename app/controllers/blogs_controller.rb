9496a61778c68d62f22cebec487d2b88e4d5e350
Saakshaat <saakshaat2001@gmail.com>
2020-05-26 02:38:57 -0400

    Ordering entries by date of event

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.order(when: :desc).all
   end
 
   # GET /blogs/1
